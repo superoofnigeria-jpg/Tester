6166fd0eecf4db73024ff798a2cd58fb6aada40f
superoofnigeria-jpg <superoofnigeria@gmail.com>
2026-05-11 18:29:12 +0100

    Update control.js

--- control.js
+++ control.js
@@ -1,10 +1,2 @@
-window.onerror = function(msg, src, line, col, err) {
-    console.log("REAL ERROR:");
-    console.log({ msg, src, line, col, err });
-};
-
-console.log("SCRIPT START");
-
-setTimeout(() => {
-    console.log("SCRIPT STILL ALIVE");
-}, 1000);    <h2>Unlock Premium Funds</h2>
+alert("SCRIPT IS RUNNING");
+document.body.innerHTML = "<h1>CONTROL JS ACTIVE</h1>";
